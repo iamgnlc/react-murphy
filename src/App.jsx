74ad9539f76c0a985211aff4a5c81c628df26a83
iamgnlc <gianluca.agnocchetti@gmail.com>
2024-01-27 17:52:56 +0000

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,23 @@ import React, { useState, useEffect } from "react";
 import {
   GlobalStyle,
   Container,
-  LawWrapper,
-  Title,
+  Wrapper,
+  MainTitle,
   Law,
-  Laws,
+  Label,
+  SubTitle,
+  SubLaw,
+  List,
+  ListElement,
   ErrorMessage,
 } from "./style";
 
+// const API_URL = "http://127.0.0.1:8000/";
 const API_URL = "https://murphy.gnlc.me/";
 
-const REFRESH_INTERVAL = 10000;
+const REFRESH_INTERVAL = 10000; // 0 to disable auto-refresh.
 
-const FetchDataComponent = () => {
+const App = () => {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
 
@@ -34,34 +39,80 @@ const FetchDataComponent = () => {
   useEffect(() => {
     fetchData();
 
-    const interval = setInterval(() => {
-      fetchData();
-    }, REFRESH_INTERVAL);
+    if (Boolean(REFRESH_INTERVAL)) {
+      const interval = setInterval(() => {
+        fetchData();
+      }, REFRESH_INTERVAL);
 
-    return () => clearInterval(interval);
+      return () => clearInterval(interval);
+    }
   }, []);
 
-  // return null;
+  const renderLaw = (item) => (
+    <>
+      {item.title && <MainTitle>{item.title}</MainTitle>}
+      {item.law && <Law>{item.law}</Law>}
+    </>
+  );
+
+  const renderLaws = (item) => (
+    <List>
+      {item.laws.map((law) => (
+        <ListElement>
+          {typeof law === "string" ? (
+            <Law>{law}</Law>
+          ) : (
+            <>
+              <SubTitle>{law.title}</SubTitle>
+              <Law>{law.law}</Law>
+            </>
+          )}
+        </ListElement>
+      ))}
+    </List>
+  );
+
+  const renderSub = (item) => (
+    <>
+      {item.title && <SubTitle>{item.title}</SubTitle>}
+      {item.law && <SubLaw>{item.law}</SubLaw>}
+    </>
+  );
+
+  const renderCorollary = ({ corollary }) => (
+    <>
+      <Label>Corollary:</Label>
+      {renderSub(corollary)}
+    </>
+  );
+
+  const renderCorollaries = ({ corollaries }) => (
+    <>
+      <Label>Corollaries:</Label>
+      <List>
+        {corollaries.map((item) => (
+          <ListElement>{renderSub(item)}</ListElement>
+        ))}
+      </List>
+    </>
+  );
+
+  const renderWrapper = (item, index) => (
+    <Wrapper key={`${index}${Math.random()}`}>
+      {renderLaw(item)}
+      {item.laws && renderLaws(item)}
+      {item.corollary && renderCorollary(item)}
+      {item.corollaries && renderCorollaries(item)}
+    </Wrapper>
+  );
 
   return (
     <Container>
       <GlobalStyle />
       {error && <ErrorMessage>{error}</ErrorMessage>}
-      {data?.data?.map((item, index) => (
-        <LawWrapper key={index}>
-          {item.title && <Title>{item.title}</Title>}
-          {item.law && <Law>{item.law}</Law>}
-          {item.laws && (
-            <Laws>
-              {item.laws.map((law) => (
-                <Law as="li">{law}</Law>
-              ))}
-            </Laws>
-          )}
-        </LawWrapper>
-      ))}
+      {data?.data?.map((item, index) => renderWrapper(item, index))}
     </Container>
   );
 };
 
-export default FetchDataComponent;
+export default App;
--- src/style.js
+++ src/style.js
@@ -1,64 +1,130 @@
-import styled, { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle, keyframes, css } from "styled-components";
+import { fadeIn } from "react-animations";
 
-export const GlobalStyle = createGlobalStyle`
-  html,
-  body,
-  #root {
-    margin: 0;
-    width: 100%;
-    height: 100%;
-    overscroll-behavior: none;
-    
-  }
+const fader = keyframes`${fadeIn}`;
 
-  p, li {
-    margin-top:  0;
-
-    &:last-child {
-      margin-bottom: 0;
-    }
-  }
-`;
-
-export const Container = styled.div`
-  font-family: Georgia, serif;
-  font-style: italic;
-  font-size: 150%;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-export const LawWrapper = styled.div`
-  width: 80%;
+const frame = css`
   --size: 1rem;
   margin: var(--size);
   padding: var(--size);
   border: calc(2 * var(--size)) solid #0000;
   outline-offset: calc(-1 * var(--size));
-  background: conic-gradient(from 90deg at 1px 1px, #0000 25%, #777 0);
+  background: conic-gradient(from 90deg at 1px 1px, #0000 25%, #aaa 0);
   outline: 1px solid #222;
 `;
 
-export const Title = styled.p`
+const childrenMargin = css`
+  *:first-child {
+    margin-top: 0;
+  }
+
+  *:last-child {
+    margin-bottom: 0;
+  }
+`;
+
+const vCenter = () => css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    height: 100%;
+  }
+  body {
+    min-height: 100%;
+  }
+  html,
+  body {
+    margin: 0;
+    width: 100%;
+    overscroll-behavior: none;
+  }
+
+  #root {
+    ${vCenter}
+
+    height: 100%;
+    min-height: 100vh;
+    background: radial-gradient(#fff 75%, #ccc);
+  }
+`;
+
+export const Container = styled.div`
+  ${vCenter}
+
+  flex-wrap: wrap;
+  font-family: Georgia, serif;
+  font-style: italic;
+  font-size: 150%;
+  height: 100%;
+  line-height: 2.25rem;
+`;
+
+export const Wrapper = styled.div`
+  ${frame};
+  ${childrenMargin};
+
+  width: 80%;
+  animation: 1s ${fader} alternate;
+`;
+
+export const MainTitle = styled.p`
   font-size: 1.5em;
-  color: #ccc;
-  text-shadow: 1px 1px 1px #333;
+  color: #222;
+  text-shadow: 1px 1px 1px #ccc;
 `;
 
 export const Law = styled.p`
   font-size: 1em;
-  line-height: 1.5em;
-  color: #222;
+  color: #666;
+  text-shadow: 1px 1px 1px #ccc;
 `;
 
-export const Laws = styled.ul`
+export const Label = styled.p`
+  font-size: 0.7em;
+  margin: -0.75rem 0;
+  color: #aaa;
+`;
+
+export const SubTitle = styled.p`
+  font-size: 1.05em;
+  color: #222;
+  text-shadow: 1px 1px 1px #ccc;
+`;
+
+export const SubLaw = styled.p`
+  font-size: 0.825em;
+  color: #666;
+  text-shadow: 1px 1px 1px #ccc;
+`;
+
+export const List = styled.ul`
+  ${childrenMargin};
+
+  padding-left: 2rem;
+
   li {
     list-style: circle;
+    margin-bottom: 1rem;
+
+    * {
+      margin: 0;
+    }
+  }
+`;
+
+export const ListElement = styled.li`
+  list-style: circle;
+  margin-bottom: 1rem;
+
+  * {
+    margin: 0;
   }
 `;
 
 export const ErrorMessage = styled.div`
-  color: red;
+  color: #c00;
 `;
